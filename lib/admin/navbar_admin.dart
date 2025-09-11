00473f87724390c13ed55b0c4c71823630610fba
Peehu Mishra <peehumishra1308@gmail.com>
2025-09-11 17:30:07 +0530

    Create navbar_admin.dart

---
+++ lib/admin/navbar_admin.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:pixel/Screens/Calender.dart';
+import 'package:pixel/Screens/club.dart';
+import 'package:pixel/Screens/event.dart';
+import 'package:pixel/Screens/friends_screen.dart';
+import 'package:pixel/Screens/updatespage.dart';
+// import 'package:pixel/Screens/main.dart';
+import 'package:pixel/admin/Admin_home.dart';
+import 'package:pixel/admin/admin_club.dart';
+import 'package:pixel/admin/admin_specs.dart';
+import 'package:pixel/admin/adminevent.dart';
+import 'package:pixel/main.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+class Navbar_Admin extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+  final String email;
+
+
+  const Navbar_Admin({
+
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+    required this.email,
+  }) : super(key: key);
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) async {
+        if(index==0){
+        
+            Navigator.push(context,MaterialPageRoute(builder: (context)=>AdminHome(email:email)));
+          
+        
+        
+          
+        }
+        else if(index==1){
+          
+            Navigator.push(context,MaterialPageRoute(builder: (context)=>Admin_FriendsScreen(email: email,)));
+          
+          
+        }
+        else if(index==2){
+          
+            Navigator.push(context,MaterialPageRoute(builder: (context)=>Adminevent(email:email)));
+          
+        }
+        else if(index==3){
+          
+            Navigator.push(context,MaterialPageRoute(builder: (context)=>Clubs_Screen_Admin(email:email)));
+          
+        }
+        
+      },
+      // backgroundColor: Colors.transparent,
+      backgroundColor: Colors.white,
+      selectedItemColor: Colors.purple,
+      // selectedItemColor: Colors.orange,
+      unselectedItemColor: Colors.black,
+      showUnselectedLabels: true,
+      type: BottomNavigationBarType.fixed,
+      items: const[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+          
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.comment_bank_outlined),
+          label: 'Specs',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.tips_and_updates_sharp),
+          label: 'Updates',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.group),
+          label: 'Clubs',
+        ),
+        
+      ]
+    );
+  }
+}
