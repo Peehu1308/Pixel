5d7aadf0f561b877f273a0c180cdd84ebd0cd120
Peehu Mishra <peehumishra1308@gmail.com>
2026-02-19 13:03:24 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// import 'dart:html' as html;
+import 'dart:html' as html;
 import 'package:flutter/material.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
