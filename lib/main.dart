2b5fd906dc7231a5490949e47d40a9efea4d47f5
Peehu Mishra <peehumishra1308@gmail.com>
2026-02-19 12:16:43 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:html' as html;
+// import 'dart:html' as html;
 import 'package:flutter/material.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
@@ -70,50 +70,51 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       home: _amplifyConfigured
           // ? DeviceCheck(mobileHome: const LoginScreen())
-          // ?LoginScreen()
-          ? SplashScreen()
+          
+          ?LoginScreen()
+          // ? SplashScreen()
           : const Scaffold(body: Center(child: CircularProgressIndicator())),
     );
   }
 }
 
-class DeviceCheck extends StatelessWidget {
-  final Widget mobileHome;
-  const DeviceCheck({required this.mobileHome, super.key});
+// class DeviceCheck extends StatelessWidget {
+//   final Widget mobileHome;
+//   const DeviceCheck({required this.mobileHome, super.key});
 
-  bool _isMobile() {
-    final userAgent = html.window.navigator.userAgent.toLowerCase();
-    return userAgent.contains('android') ||
-        userAgent.contains('iphone') ||
-        userAgent.contains('ipad') ||
-        userAgent.contains('ipod');
-  }
+//   bool _isMobile() {
+//     final userAgent = html.window.navigator.userAgent.toLowerCase();
+//     return userAgent.contains('android') ||
+//         userAgent.contains('iphone') ||
+//         userAgent.contains('ipad') ||
+//         userAgent.contains('ipod');
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    if (_isMobile()) {
-      return mobileHome;
-    } else {
-      return const Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.mobile_off, size: 80, color: Colors.grey),
-              SizedBox(height: 20),
-              Text(
-                '⚠️ This app is available only on mobile devices.\nOpen the link on your mobile devices',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18, color: Colors.black54),
-              ),
-            ],
-          ),
-        ),
-      );
-    }
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     if (_isMobile()) {
+//       return mobileHome;
+//     } else {
+//       return const Scaffold(
+//         backgroundColor: Colors.white,
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Icon(Icons.mobile_off, size: 80, color: Colors.grey),
+//               SizedBox(height: 20),
+//               Text(
+//                 '⚠️ This app is available only on mobile devices.\nOpen the link on your mobile devices',
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(fontSize: 18, color: Colors.black54),
+//               ),
+//             ],
+//           ),
+//         ),
+//       );
+//     }
+//   }
+// }
 
 class MainScreen extends StatefulWidget {
   final String email;
